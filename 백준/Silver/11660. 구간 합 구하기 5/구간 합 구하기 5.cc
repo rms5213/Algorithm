a3196c9af033e9d133d1f95ddf172f42763bd5e8
Lee Geun Woo <63998139+rms5213@users.noreply.github.com>
2024-08-07 00:36:29 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 112 ms, Memory: 10372 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
@@ -3,37 +3,46 @@
 
 using namespace std;
 
-#define MAX 1024 + 1
+#define MAX 1024 + 10
+int arr[MAX][MAX];
+int dp[MAX][MAX];
 
 int main() {
-    ios_base :: sync_with_stdio(false); 
-    cin.tie(NULL); 
-    cout.tie(NULL);    
-
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    
     int N, M;
-    int x1, y1, x2, y2;
     cin >> N >> M;
-    int prefix[MAX][MAX];
-    int val1, val2;
-    int sum1 = 0, sum2[MAX];
-    for(int i = 0 ; i < N ; i++){
-        for(int j = 0 ; j < N ; j++){
-            cin >> val1;
-            sum1+=val1;
-            prefix[i+1][j+1] = sum1 + prefix[i][j+1];
+    
+    
+    
+    for(int i = 1 ; i <= N ; i++){
+        for(int j = 1 ; j <= N ; j++){
+            cin >> arr[i][j];
         }
-        sum1=0;
     }
     
-    int res[100000];
 
-    for(int i = 0 ; i < M ; i++){
-            cin >> x1 >> y1 >> x2 >> y2;
-            res[i] = prefix[x2][y2] - prefix[x2][y1-1] - prefix[x1-1][y2] + prefix[x1-1][y1-1];
-    }
-    for(int i = 0 ; i < M ; i++){
-        cout << res[i] << '\n';            
-    }
-
+    dp[1][1] = arr[1][1];
     
+    for(int i = 2 ; i <= N ; i++) {
+        dp[1][i] = dp[1][i-1] + arr[1][i];
+        dp[i][1] = dp[i-1][1] + arr[i][1];
+    }
+    
+    for(int i = 2 ; i <= N ; i++){
+        for(int j = 2 ; j <= N ; j++){
+            dp[i][j] = arr[i][j] + dp[i][j-1] + dp[i-1][j] - dp[i-1][j-1];
+        }
+    }
+    
+    for(int i = 0 ; i < M ; i++){
+        int x1, y1, x2, y2;
+        cin >> x1 >> y1 >> x2 >> y2;
+        
+        cout << dp[x2][y2] - dp[x2][y1-1] - dp[x1-1][y2] + dp[x1-1][y1-1] << "\n";
+    }
+    
+    
+    return 0;
 }
